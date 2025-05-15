86aa8535e7a6692508c4766c64abeab5a419fe8d
Cory Farmer <cory@Corys-MacBook-Pro.local>
2025-05-15 09:55:28 -0400

    cleanup bat-extras installation

--- bootstrap/install_bat_extras.sh
+++ bootstrap/install_bat_extras.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 git clone https://github.com/eth-p/bat-extras.git "$HOME"/tmp/bat-extras
-cd "$HOME"/tmp/bat-extras
-./build.sh --install --prefix="$HOME"/.local
-cd ../
+"$HOME"/tmp/bat-extras/build.sh --install --prefix="$HOME"/.local
 rm -rf "$HOME"/tmp/bat-extras
 
