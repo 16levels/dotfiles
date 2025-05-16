50796309c3e4f297e5be1228d48bb10443979c56
Cory Farmer <cory@Corys-MacBook-Pro.local>
2025-05-16 11:06:08 -0400

    add jellyfin bootstrap

---
+++ bootstrap/setup_jellyfin_container.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+useradd -c 'Jellyfin media server' jellyfin
+su -c "mkdir -p \
+	/home/jellyfin/config \
+	/home/jellyfin/cache \
+	/home/jellyfin/.config/containers/systemd" jellyfin
+
+su -c 'echo \
+"[Unit]
+Description=Jellyfin
+
+[Container]
+Image=docker.io/jellyfin/jellyfin:latest
+AutoUpdate=registry
+PublishPort=8096:8096/tcp
+Network=host
+UserNS=keep-id
+AddDevice=/dev/dri:/dev/dri
+Volume=/home/jellyfin/config:/config:Z
+Volume=/home/jellyfin/cache:/cache:Z
+Volume=/mnt/jellyfin:/media:Z
+
+[Service]
+SuccessExitStatus=0 143
+ 
+[Install]
+WantedBy=default.target" > /home/jellyfin/.config/containers/systemd/jellyfin.container' jellyfin
+
+
+loginctl enable-linger jellyfin
+systemctl --machine=jellyfin@.host --user daemon-reload
+systemctl --machine=jellyfin@.host --user start jellyfin.service
+systemctl --machine=jellyfin@.host --user enable --now podman-auto-update.timer
+
+usermod -s /usr/sbin/nologin jellyfin
+passwd -l jellyfin
+
+firewall-cmd --permanent --add-port=8096/tcp
+firewall-cmd --reload
+
