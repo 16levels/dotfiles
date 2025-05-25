2f591febde265c6e5ac1d5343881a8ecafe42d63
Cory Farmer <cory@Corys-MacBook-Pro.local>
2025-05-25 07:33:30 -0400

    Gnome Text Editor customizations

---
+++ bootstrap/gnome_text_editor_settings.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+theme_url='https://raw.githubusercontent.com/Fausto-Korpsvart/Catppuccin-GTK-Theme/refs/heads/main/extra/text-editor/catppuccin-frappe.xml'
+
+curl --create-dirs --output-dir "$HOME/.var/app/org.gnome.TextEditor/data/gtksourceview-5/styles" -O $theme_url
+
+flatpak run --command=gsettings org.gnome.TextEditor set org.gnome.TextEditor style-scheme 'catppuccin-frappé'
+flatpak run --command=gsettings org.gnome.TextEditor set org.gnome.TextEditor custom-font 'Iosevka 14'
+flatpak run --command=gsettings org.gnome.TextEditor set org.gnome.TextEditor show-line-numbers true
+flatpak run --command=gsettings org.gnome.TextEditor set org.gnome.TextEditor keybindings 'vim'
+flatpak run --command=gsettings org.gnome.TextEditor set org.gnome.TextEditor show-map true
