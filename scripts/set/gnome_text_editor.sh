4666d6da78f60ef8650d4a7b88a1cb30633f7231
Cory Farmer <cory@lunchbox.local>
2025-07-15 13:26:12 -0400

    Add dark/light mode to gnome-text-editor theme.

--- scripts/set/gnome_text_editor.sh
+++ scripts/set/gnome_text_editor.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-theme_url='https://raw.githubusercontent.com/Fausto-Korpsvart/Catppuccin-GTK-Theme/refs/heads/main/extra/text-editor/catppuccin-frappe.xml'
+#theme_url='https://raw.githubusercontent.com/Fausto-Korpsvart/Catppuccin-GTK-Theme/refs/heads/main/extra/text-editor/catppuccin-frappe.xml'
 
-curl --create-dirs --output-dir "$HOME/.var/app/org.gnome.TextEditor/data/gtksourceview-5/styles" -O $theme_url
+#curl --create-dirs --output-dir "$HOME/.var/app/org.gnome.TextEditor/data/gtksourceview-5/styles" -O $theme_url
 
-flatpak run --command=gsettings org.gnome.TextEditor set org.gnome.TextEditor style-scheme 'catppuccin-frappé'
+flatpak run --command=gsettings org.gnome.TextEditor set org.gnome.TextEditor style-scheme 'catppuccin'
 flatpak run --command=gsettings org.gnome.TextEditor set org.gnome.TextEditor custom-font 'Iosevka 14'
 flatpak run --command=gsettings org.gnome.TextEditor set org.gnome.TextEditor show-line-numbers true
 flatpak run --command=gsettings org.gnome.TextEditor set org.gnome.TextEditor keybindings 'vim'
