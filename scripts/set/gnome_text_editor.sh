0ed939afaf3393965bc78cf203a0e23f9b18e0cf
Cory Farmer <cory@lunchbox.local>
2025-07-15 21:45:25 -0400

    Remove 'batman' alias. Change 'distrobox.ini' & 'gnome_text_editor.sh'.

--- scripts/set/gnome_text_editor.sh
+++ scripts/set/gnome_text_editor.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-#theme_url='https://raw.githubusercontent.com/Fausto-Korpsvart/Catppuccin-GTK-Theme/refs/heads/main/extra/text-editor/catppuccin-frappe.xml'
 
-#curl --create-dirs --output-dir "$HOME/.var/app/org.gnome.TextEditor/data/gtksourceview-5/styles" -O $theme_url
-
-flatpak run --command=gsettings org.gnome.TextEditor set org.gnome.TextEditor style-scheme 'catppuccin'
-flatpak run --command=gsettings org.gnome.TextEditor set org.gnome.TextEditor custom-font 'Iosevka 14'
-flatpak run --command=gsettings org.gnome.TextEditor set org.gnome.TextEditor show-line-numbers true
-flatpak run --command=gsettings org.gnome.TextEditor set org.gnome.TextEditor keybindings 'vim'
-flatpak run --command=gsettings org.gnome.TextEditor set org.gnome.TextEditor show-map true
+echo "style-scheme 'catppuccin' \
+	custom-font 'Iosevka 12' \
+	show-line-numbers true \
+	keybindings 'vim' \
+	show-map true" | \
+	xargs -n2 flatpak run --command=gsettings org.gnome.TextEditor set org.gnome.TextEditor
