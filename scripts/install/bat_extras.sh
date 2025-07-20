3998f46de8d33d0c0d979e2d4b7ea831c6c23e43
Cory Farmer <cory@lunchbox.local>
2025-07-19 21:53:01 -0400

    remove scripts for ansible conversion

--- scripts/install/bat_extras.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-git clone https://github.com/eth-p/bat-extras.git "$HOME"/tmp/bat-extras
-"$HOME"/tmp/bat-extras/build.sh --install --prefix="$HOME"/.local
-rm -rf "$HOME"/tmp/bat-extras
-find "$HOME"/tmp -type d -empty -delete
--- scripts/set/gnome_text_editor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-  echo "style-scheme 'catppuccin' \
-	custom-font 'Iosevka 12' \
-	show-line-numbers true \
-	keybindings 'vim' \
-	show-map true" | \
-	xargs -n2 flatpak run --command=gsettings org.gnome.TextEditor set org.gnome.TextEditor
