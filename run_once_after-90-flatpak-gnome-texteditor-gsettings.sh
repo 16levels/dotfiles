c37025289e2a406dbd275c05e57f5d20749f4529
Cory Farmer <mr@cory.farm>
2025-08-16 15:06:39 -0400

    remove one time script

--- run_once_after-90-flatpak-gnome-texteditor-gsettings.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cat <<EOF | xargs -n2 flatpak run --command=gsettings org.gnome.TextEditor set org.gnome.TextEditor
-style-scheme 'catppuccin'
-custom-font 'Iosevka 12'
-show-line-numbers true
-keybindings 'vim'
-show-map true 
-EOF
