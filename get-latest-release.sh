3a4c9d19e6f4f8f149ff6be2bfcdc3ac165d06df
Cory Farmer <mr@cory.farm>
2025-08-20 09:55:33 -0400

    account for installs without curl available

--- get-latest-release.sh
+++ get-latest-release.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-curl -s "https://api.github.com/repos/$1/releases/latest" | grep tag_name | cut -d : -f2 | tr -d "v\", "
+if command -v curl > /dev/null 2>&1; then
+	https_cmd="curl -s"
+elif command -v wget > /dev/null 2>&1; then
+	https_cmd="wget -qO -"
+else exit 1; fi
+        
+$https_cmd "https://api.github.com/repos/$1/releases/latest" | grep tag_name | cut -d : -f2 | tr -d "v\", "
