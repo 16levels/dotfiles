e54f0f7f2b18790a2564b301a87f4589a01b3780
Cory Farmer <mr@cory.farm>
2025-08-20 09:54:58 -0400

    fix redirection

--- run_once_after_25-rebuild-bat-cache.sh
+++ run_once_after_25-rebuild-bat-cache.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 echo "Rebuilding 'bat' cache..."
-if command -v bat 2>&1 /dev/null; then
+if command -v bat > /dev/null 2>&1; then
 	bat cache --build
-elif command -v batcat 2>&1 /dev/null; then
+elif command -v batcat > /dev/null 2>&1; then
 	batcat cache --build
 else echo "'bat' could not be found..."; fi
