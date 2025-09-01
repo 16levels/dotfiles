9ef4451c4e5ecc153714ba5550e4bd6a3ffd5fba
Cory Farmer <mr@cory.farm>
2025-09-01 15:41:01 -0400

    remove syncthing binary package, add syncthing quadlet

--- dot_config/fish/conf.d/aliases.fish
+++ dot_config/fish/conf.d/aliases.fish
@@ -15,9 +15,12 @@ switch (uname)
 		alias gh="toolbox run gh"
 		alias go="toolbox run go"
 		alias git="toolbox run git"
+		alias hugo="toolbox run hugo"
+		alias make="toolbox run make"
 		alias opam="toolbox run opam"
-		alias utop="toolbox run opam exec utop"
+		alias guile="toolbox run guile"
 		alias lazygit="toolbox run lazygit"
+		alias utop="toolbox run opam exec utop"
 		
 	end
 
