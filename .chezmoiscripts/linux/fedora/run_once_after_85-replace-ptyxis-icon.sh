5688a3552e9816a90db856f3f5c1ef2738021a28
Cory Farmer <mr@cory.farm>
2025-09-06 16:34:34 -0400

    change template to posix shell

---
+++ .chezmoiscripts/linux/fedora/run_once_after_85-replace-ptyxis-icon.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+TARGET_FILE="$HOME/.local/share/applications/org.gnome.Ptyxis.desktop"
+
+if [ ! -f "$TARGET_FILE" ]; then
+	echo "File $TARGET_FILE does not exist. Creating application entry..."
+	sed 's/Icon=org.gnome.Ptyxis/Icon=ptyxis/g' \
+		/usr/share/applications/org.gnome.Ptyxis.desktop > \
+	"$HOME/.local/share/applications/org.gnome.Ptyxis.desktop"
+else
+	echo "File $TARGET_FILE already exists."
+fi
