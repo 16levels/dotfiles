824a3f8b8f10069238848471e84886b2fea59de1
Cory Farmer <mr@cory.farm>
2025-09-06 18:33:28 -0400

    check for installed editors

--- .chezmoiscripts/linux/run_once_25-flatpak-sdk-override.sh
+++ .chezmoiscripts/linux/run_once_25-flatpak-sdk-override.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-echo "Enabling available SDKs for flatpak 'io.neovim.nvim'..."
-flatpak override --user --env="FLATPAK_ENABLE_SDK_EXT=*" io.neovim.nvim
-echo "Enabling available SDKs for flatpak 'org.gnu.Emacs'..."
-flatpak override --user --env="FLATPAK_ENABLE_SDK_EXT=*" org.gnu.emacs
+editors="\
+	com.helix_editor.Helix \
+	com.visualstudio.code \
+	com.vscodium.codium \
+	io.neovim.nvim \
+	org.gnu.emacs \
+	"
+
+installed_apps=$(flatpak list)
+
+for flatpak in $editors; do
+	if echo "$installed_apps" | grep -q "$flatpak"; then
+		echo "Enabling available SDKs for flatpak '$flatpak'..."
+		flatpak override --user --env="FLATPAK_ENABLE_SDK_EXT=*" "$flatpak"
+	fi
+done
