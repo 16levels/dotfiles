3824a72e90270f2ec88ad44046d31ff82ab28c91
Cory Farmer <mr@cory.farm>
2025-09-07 03:16:28 -0400

    ensure .l/b is in path before uv tool install

--- .chezmoiscripts/linux/run_once_20_install-uv.sh
+++ .chezmoiscripts/linux/run_once_20_install-uv.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# ensure .l/b is in path for package install script
+if [ -d "$HOME/.local/bin" ]; then
+    export PATH="HOME/.local/bin:$PATH"
+fi
