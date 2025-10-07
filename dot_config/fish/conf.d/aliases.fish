2d7d10a6dc53bf8363ecd76507964d94c10722d5
Cory Farmer <mr@cory.farm>
2025-10-07 17:38:40 -0400

    adjust aliases

--- dot_config/fish/conf.d/aliases.fish
+++ dot_config/fish/conf.d/aliases.fish
@@ -10,18 +10,17 @@ switch (uname)
         # Linux Specific
         alias ollama="podman exec -it ollama ollama"
 
-        # Integrate toolbx commands with host
-        if not [ $hostname = toolbx ]
-            set toolbx_cmds 7z cosign gdu go guile3.0 hugo just lazygit npm opam
-            for cmd in $toolbx_cmds
-                alias "$cmd=toolbox run $cmd"
-            end
-
-        # Access host commands from within toolbx containers
-        else
+        # Access host commands within toolbx Containers
+        if [ $hostname = toolbx ]
             set host_cmds tailscale
             for cmd in $host_cmds
                 alias "$cmd=host-spawn $cmd"
+            end
+        # Integrate toolbx commands outside of container
+        else if command -q toolbox
+            set toolbx_cmds 7z 7zz cosign gdu go guile hugo just lazygit npm opam
+            for cmd in $toolbx_cmds
+                command -q "$cmd"; or alias "$cmd=toolbox run $cmd"
             end
         end
 
