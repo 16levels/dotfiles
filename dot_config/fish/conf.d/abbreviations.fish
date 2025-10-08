3339349ed726730961531efcf0ba65e17c7350a1
Cory Farmer <mr@cory.farm>
2025-10-07 21:40:02 -0400

    adjust abbreviations

--- dot_config/fish/conf.d/abbreviations.fish
+++ dot_config/fish/conf.d/abbreviations.fish
@@ -30,7 +30,7 @@ fish_add_path -ag "$HOME/.local/share/nvim/mason/bin"
 
 # asciidoctor container
 for cmd in asciidoctor{-epub3,-pdf,-revealjs,}
-    abbr -a "$cmd" podman run --rm -it -v="\$PWD:/documents/:z" docker.io/asciidoctor/docker-asciidoctor "$cmd"
+    command -q "$cmd"; or abbr -a "$cmd" podman run --rm -it -v="\$PWD:/documents/:z" docker.io/asciidoctor/docker-asciidoctor "$cmd"
 end
 
 # hercules container
