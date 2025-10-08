083110436c7468edb8523e0098660bc0273190e4
Cory Farmer <mr@cory.farm>
2025-10-08 15:46:44 -0400

    fix scope

--- dot_config/fish/conf.d/toolbx-aliases.fish
+++ dot_config/fish/conf.d/toolbx-aliases.fish
@@ -1,18 +1,16 @@
 # Toolbx Aliases
 
-# $host_cmds: access host commands within toolbx containers
-set host_cmds tailscale
-
 if [ $hostname = toolbx ]
+    # $host_cmds: access host commands within toolbx containers
+    set host_cmds tailscale
     for cmd in $host_cmds
-	alias "$cmd=host-spawn $cmd"
+        alias "$cmd=host-spawn $cmd"
     end
 
-# $toolbx_cmds: integrate toolbx commands outside of container
-set toolbx_cmds gdu go hugo just lazygit npm opam
-
 else if command -q toolbox; and [ -e /run/ostree-booted ]
+    # $toolbx_cmds: integrate toolbx commands outside of container
+    set toolbx_cmds gdu go hugo just lazygit npm opam
     for cmd in $toolbx_cmds
-	command -q "$cmd"; or alias "$cmd=toolbox run $cmd"
+        command -q "$cmd"; or alias "$cmd=toolbox run $cmd"
     end
 end
