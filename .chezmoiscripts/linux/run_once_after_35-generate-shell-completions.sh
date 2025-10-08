1bb3a238d57abb19c61951b47c24505045aac313
Cory Farmer <mr@cory.farm>
2025-10-08 12:39:49 -0400

    generate shell completions

---
+++ .chezmoiscripts/linux/run_once_after_35-generate-shell-completions.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "Generating shell completions for external binaries..."
+chezmoi completion fish > ~/.config/fish/completions/chezmoi.fish
+devpod completion fish > ~/.config/fish/completions/devpod.fish
+glow completion fish > ~/.config/fish/completions/glow.fish
+rg --generate=complete-fish > ~/.config/fish/completions/rg.fish
+starship completions fish > ~/.config/fish/completions/starship.fish
