9f8198752ca87b4c46b188f4aa92f6ba6bd810ff
Cory Farmer <mr@cory.farm>
2025-12-15 09:12:58 -0500

    adjust hercules container abbreviation

--- dot_config/fish/conf.d/hercules-container.fish
+++ dot_config/fish/conf.d/hercules-container.fish
@@ -4,8 +4,8 @@ abbr -a --set-cursor=! hercules podman run --rm -it \
     --cap-add=NET_ADMIN,SYS_NICE \
     --userns=keep-id \
     -p 3270:3270 -p 8038:8038 \
-    -v "\$PWD!:/home/hercules/:z" \
-    -w /home/hercules/ \
+    -v "\$PWD!:/hercules/:z" \
+    -w /hercules/ \
     -e HERCULES_CNF="hercules.cnf" \
     -e HERCULES_RC="hercules.rc" \
     ghcr.io/16levels/hercules
